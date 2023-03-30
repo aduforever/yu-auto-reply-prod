2b35d7228869a2f3d0e03f026f3fa7726f7af834
lienhui <lienhui@cherish.com.cn>
2023-03-30 18:32:16 +0800

    提交代码

--- src/main/java/com/yupi/autoreply/controller/MainController.java
+++ src/main/java/com/yupi/autoreply/controller/MainController.java
@@ -1,7 +1,15 @@
 package com.yupi.autoreply.controller;
 
+import com.yupi.autoreply.answerer.Answerer;
+import com.yupi.autoreply.factory.AnswererFactory;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -15,4 +23,13 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class MainController {
 
+    @ApiOperation(value="读取写操作记录接口", notes="读取写操作记录接口")
+    @PostMapping(value = "/answer")
+    public String answer(String question,HttpServletRequest req) throws Exception {
+        Answerer answerer = AnswererFactory.createAnswerer("openai");
+        log.info("收到新提问 \n 问题：{}", question);
+        // 2. 获取回答
+        String answer = answerer.doAnswer(question);
+        return answer;
+    }
 }
